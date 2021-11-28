1f1a6ed5eba0c445ac5d03c7141f00eeb1c59f0a
doni63 <dbatistajunior63@gmail.com>
2021-11-28 10:01:08 -0300

    Commit apartir do intllij

---
+++ src/br/com/dio/Program.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!");
+        int a = 10;
+        int b = 20;
+        int soma = a + b;
+        System.out.println("soma: "+ soma);
+    }
+}
